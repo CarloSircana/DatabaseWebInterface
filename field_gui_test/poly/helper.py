a89e9ab41ed0117849eb1ccff7037bdc20c6fe5a
mir-pias <mir.pias01@gmail.com>
2021-08-27 03:14:58 +0200

    add discriminant,cm,signature search

--- field_gui_test/poly/helper.py
+++ field_gui_test/poly/helper.py
@@ -1,40 +1,167 @@
+from .models import Field
 
-def format_polynomials(input_degree, polys):
-    ## generate list_of_x for polynomial
-    input_degree = int(input_degree)
-    superscript = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
-    list_of_x = []
-    for i in range(input_degree+1):
-        if i >= 2:
-            list_of_x.append(f'x{i}'.translate(superscript))
-        elif i ==1 or i == -1:
-            list_of_x.append('x')
-        else:
-            list_of_x.append('')
-    list_of_x = list_of_x[::-1]
-    
-    ## put the coeffs(polys) and list_of_x together to form output string
-    output_list = []
-    for i in range(len(polys)):
-        poly_str = []
-        for j in range(len(list_of_x )):
-            if polys[i][j] != 0:
-                if polys[i][j] >= 2 and j != 0:
-                    poly_str.append("+" + str(polys[i][j])+list_of_x[j])
-                elif polys[i][j] >= 2 and j == 0:
-                    poly_str.append(str(polys[i][j])+list_of_x[j])
-                elif polys[i][j] == 1 and list_of_x[j] != '' and j != 0:
-                    poly_str.append("+" +list_of_x[j])
-                elif polys[i][j] == 1 and list_of_x[j] != '' and j == 0:
-                    poly_str.append(list_of_x[j])
-                elif list_of_x[j] == '':
-                    poly_str.append("+" +str(polys[i][j]))
-                elif polys[i][j] == -1 and list_of_x[j] != '':
-                    poly_str.append("-" +list_of_x[j])
-                elif list_of_x[j] == '':
-                    poly_str.append(str(polys[i][j]))
+class Helper():
+
+    def __init__(self) -> None:
+        pass
+    def format_polynomials(self, input_degree, polys):
+        ## generate list_of_x for polynomial
+        input_degree = int(input_degree)
+        superscript = str.maketrans("0123456789", "⁰¹²³⁴⁵⁶⁷⁸⁹")
+        list_of_x = []
+        for i in range(input_degree+1):
+            if i >= 2:
+                list_of_x.append(f'x{i}'.translate(superscript))
+            elif i ==1 or i == -1:
+                list_of_x.append('x')
+            else:
+                list_of_x.append('')
+        list_of_x = list_of_x[::-1]
+        
+        ## put the coeffs(polys) and list_of_x together to form output string
+        output_list = []
+        for i in range(len(polys)):
+            poly_order = list(polys[i])
+            poly_order = poly_order[::-1]
+            #print(poly_order)
+            poly_str = []
+            for j in range(len(list_of_x )):
+                if poly_order[j] != 0:
+                    if poly_order[j] >= 2 and j != 0:
+                        poly_str.append("+" + str(poly_order[j])+list_of_x[j])
+                    elif poly_order[j] >= 2 and j == 0:
+                        poly_str.append(str(poly_order[j])+list_of_x[j])
+                    elif poly_order[j] == 1 and list_of_x[j] != '' and j != 0:
+                        poly_str.append("+" +list_of_x[j])
+                    elif poly_order[j] == 1 and list_of_x[j] != '' and j == 0:
+                        poly_str.append(list_of_x[j])
+                    elif list_of_x[j] == '' and poly_order[j] > 0:
+                        poly_str.append("+" +str(poly_order[j]))
+                    elif poly_order[j] == -1 and list_of_x[j] != '':
+                        poly_str.append("-" +list_of_x[j])
+                    elif list_of_x[j] == '':
+                        poly_str.append(str(poly_order[j]))
+                    else:
+                        poly_str.append(str(poly_order[j])+list_of_x[j])
+            output_list.append(''.join(poly_str))
+        
+        #output_list = output_list[::-1]
+        return output_list 
+
+
+    def degree_(self, input_degree, input_rm = None):
+        if ',' in input_degree:
+            input_degree_range = input_degree.split(',')
+            input_degree_range = list(map(int, input_degree_range))
+
+            degree_range = list(range(input_degree_range[0], input_degree_range[1]+1))
+            output_list = []
+            for i in degree_range:
+                if input_rm  == None:
+                    polys = Field.objects.values_list('polynomial', flat=True).filter(degree = i)
                 else:
-                    poly_str.append(str(polys[i][j])+list_of_x[j])
-        output_list.append(''.join(poly_str))
+                    polys = Field.objects.values_list('polynomial', flat=True).filter(degree = i).filter(real_embeddings = input_rm)
+                formatted_poly = self.format_polynomials(i,polys)
+                output_list = output_list + formatted_poly
+
+        else:
+            #input_degree = int(input_degree)
+            if input_rm  == None:
+                polys = Field.objects.values_list('polynomial', flat=True).filter(degree = input_degree)
+            else:
+                polys = Field.objects.values_list('polynomial', flat=True).filter(degree = input_degree).filter(real_embeddings = input_rm)
+            output_list = self.format_polynomials(input_degree,polys)
+
+        return output_list
     
-    return output_list 
+    def disc_(self, input_disc):
+        if ',' in input_disc:
+            input_disc_range = input_disc.split(',')
+            #input_disc_range = list(map(int, input_disc_range))
+
+            #disc_range = list(range(input_disc_range[0], input_disc_range[1]+1))
+            #print(disc_range)
+            output_list = []
+            #for i in disc_range:
+            polys = Field.objects.values_list('polynomial','degree').filter(discriminant__range = (input_disc_range[0], input_disc_range[1]))
+            for i in range(len(polys)):
+                formatted_poly = self.format_polynomials(polys[i][-1],polys[i][:-1])
+                output_list = output_list + formatted_poly
+            
+            return output_list
+
+        else:
+            #input_degree = int(input_degree)
+            output_list = []
+            polys = Field.objects.values_list('polynomial','degree').filter(discriminant = input_disc)
+            for i in range(len(polys)):
+                formatted_poly = self.format_polynomials(polys[i][-1],polys[i][:-1])
+                output_list = output_list + formatted_poly
+            #output_list = self.format_polynomials(input_degree,polys)
+
+        return output_list
+
+    def degree_disc_(self, input_disc,input_degree):
+        if ',' in input_disc and ',' not in input_degree:
+            input_disc_range = input_disc.split(',')
+            #input_disc_range = list(map(int, input_disc_range))
+
+            polys = Field.objects.values_list('polynomial',flat=True).filter(degree = input_degree)
+            polys = polys.filter(discriminant__range = (input_disc_range[0], input_disc_range[1]))
+            
+            output_list = self.format_polynomials(input_degree, polys)
+            
+            return output_list
+
+        elif ',' not in input_disc and ',' in input_degree:
+            input_degree_range = input_degree.split(',')
+            input_degree_range = list(map(int, input_degree_range))
+
+            degree_range = list(range(input_degree_range[0], input_degree_range[1]+1))
+            output_list = []
+            for i in degree_range:
+                polys = Field.objects.values_list('polynomial', flat=True).filter(degree = i).filter(discriminant = input_disc)
+                formatted_poly = self.format_polynomials(i,polys)
+                output_list = output_list + formatted_poly
+
+            return output_list
+
+        elif ',' in input_disc and ',' in input_degree:
+            input_degree_range = input_degree.split(',')
+            input_degree_range = list(map(int, input_degree_range))
+            input_disc_range = input_disc.split(',')
+
+            degree_range = list(range(input_degree_range[0], input_degree_range[1]+1))
+            output_list = []
+            for i in degree_range:
+                polys = Field.objects.values_list('polynomial', flat=True).filter(degree = i)
+                polys = polys.filter(discriminant__range = (input_disc_range[0], input_disc_range[1]))
+                formatted_poly = self.format_polynomials(i,polys)
+                output_list = output_list + formatted_poly
+
+            return output_list
+        elif ',' not in input_degree and ',' not in input_disc:
+            polys = Field.objects.values_list('polynomial',flat=True).filter(degree = input_degree)
+            polys = polys.filter(discriminant = input_disc)
+            output_list = self.format_polynomials(input_degree, polys)
+            
+            return output_list
+
+    def cm_(self,input_cm):
+        polys = Field.objects.values_list('polynomial','degree').filter(cm = input_cm)
+        output_list = []
+        for i in range(len(polys)):
+                formatted_poly = self.format_polynomials(polys[i][-1],polys[i][:-1])
+                output_list = output_list + formatted_poly
+            
+        return output_list
+
+
+    def signature_(self,input_rm,input_s):
+        deg = int(input_rm) + 2*int(input_s)
+
+        polys = Field.objects.values_list('polynomial', flat=True).filter(degree = deg).filter(real_embeddings = input_rm)
+
+        output_list = self.format_polynomials(deg,polys)
+
+        return output_list
--- field_gui_test/poly/views.py
+++ field_gui_test/poly/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-from .helper import format_polynomials
+from .helper import Helper
 from .models import Field
 
 def index(request):
@@ -16,27 +16,33 @@ def index(request):
 
 def output(request):
     input_degree = request.GET.get('degree')
+    input_disc = request.GET.get('discriminant')
+    input_cm = request.GET.get('cm')
+    input_sig = request.GET.get('signature')
+
+    #print(request.GET)
     if input_degree == '' or input_degree == None:
-        return render(request, 'poly/index.html')
+        if input_disc == '' or input_disc == None:
+            if input_cm == '' or input_cm == None:
+                if input_sig == '' or input_sig == None:
+                    return render(request, 'poly/index.html')
+    
+    sig = input_sig.split(',')
 
-    if ',' in input_degree:
-        input_degree_range = input_degree.split(',')
-        input_degree_range = list(map(int, input_degree_range))
+    poly = Helper()
 
-        degree_range = list(range(input_degree_range[0], input_degree_range[1]+1))
-        output_list = []
-        for i in degree_range:
-            polys = Field.objects.values_list('polynomial', flat=True).filter(degree = i)
-            formatted_poly = format_polynomials(i,polys)
-            output_list = output_list + formatted_poly
-
+    if input_degree != '' and input_disc == '':
+        output_list = poly.degree_(input_degree)
+    elif input_degree == '' and input_disc != '':
+        output_list = poly.disc_(input_disc)
+    elif input_degree != '' and input_disc != '':
+        output_list = poly.degree_disc_(input_disc,input_degree)
     else:
-        #input_degree = int(input_degree)
-        polys = Field.objects.values_list('polynomial', flat=True).filter(degree = input_degree)
-        output_list = format_polynomials(input_degree,polys)
+        output_list = poly.signature_(sig[0],sig[1])
+    
 
     #print(output_list)
-    context = {'degree': input_degree, 'queryset': output_list}
+    context = {'degree': input_degree, 'discriminant':input_disc, 'queryset': output_list}
     #print((polys[0]))
     #return HttpResponse(output)
     return render(request, 'poly/output.html',context)
