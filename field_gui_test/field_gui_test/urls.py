2634a5bf091e4700a3282f7442cca8896c666776
mir-pias <mir.pias01@gmail.com>
2023-03-30 16:37:33 +0200

    ignore pyc + fix urls

--- field_gui_test/field_gui_test/urls.py
+++ field_gui_test/field_gui_test/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('poly/', include('poly.urls')),
+    path('', include('poly.urls')),
     path('admin/', admin.site.urls),
 ]
